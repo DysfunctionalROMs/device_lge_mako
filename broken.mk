ee0f0eef17546183111966360044aa081046071b
CrackerTeg <crackerteg@gmail.com>
2015-07-22 17:32:56 -0500

    Broke it

--- broken.mk
+++ broken.mk
@@ -2,21 +2,28 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/broken/config/gsm.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := mako
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := broken_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY47V/1836172:user/release-keys \
@@ -24,4 +31,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
-
